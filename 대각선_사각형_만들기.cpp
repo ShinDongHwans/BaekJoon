1cb3f055d02a38089e13b52649fc0c7cfad1fa32
Nights <sdhqaz3034@gmail.com>
2019-11-06 07:55:36 +0000

    Upload New File

---
+++ 대각선_사각형_만들기.cpp
@@ -0,0 +1,78 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int i, j;
+	scanf("%d", &n);
+	if(n == 3){
+		printf("***\n***\n***");
+		return 0;
+	}
+	for(i=0;i<n;i++)
+		printf("*");
+		
+	printf("\n");
+	
+	if(i%2 == 1){
+		for(i=0;i<(n-2)/2;i++){
+			printf("*");
+			for(j=0;j<i;j++)
+				printf(" ");
+			printf("*");
+			for(j=0;j<n-4-2*i;j++)
+				printf(" ");
+			printf("*");
+			for(j=0;j<i;j++)
+				printf(" ");
+			printf("*\n");				
+		}
+		printf("*");
+			for(j=0;j<i;j++)
+				printf(" ");
+			printf("*");
+			for(j=0;j<i;j++)
+				printf(" ");
+			printf("*\n");
+		for(i=(n-2)/2-1;i>=0;i--){
+			printf("*");
+			for(j=0;j<i;j++)
+				printf(" ");
+			printf("*");
+			for(j=0;j<n-4-2*i;j++)
+				printf(" ");
+			printf("*");
+			for(j=0;j<i;j++)
+				printf(" ");
+			printf("*\n");				
+		}
+	}
+	
+	else{
+		for(i=0;i<(n-2)/2;i++){
+			printf("*");
+			for(j=0;j<i;j++)
+				printf(" ");
+			printf("*");
+			for(j=0;j<n-4-2*i;j++)
+				printf(" ");
+			printf("*");
+			for(j=0;j<i;j++)
+				printf(" ");
+			printf("*\n");				
+		}
+		for(i=(n-2)/2-1;i>=0;i--){
+			printf("*");
+			for(j=0;j<i;j++)
+				printf(" ");
+			printf("*");
+			for(j=0;j<n-4-2*i;j++)
+				printf(" ");
+			printf("*");
+			for(j=0;j<i;j++)
+				printf(" ");
+			printf("*\n");				
+		}
+	}
+	
+	for(i=0;i<n;i++)
+		printf("*");
+}
