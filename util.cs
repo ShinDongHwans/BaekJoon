c72b525f2a338524cce173f4afcd6f554a7473e1
ShinDongHwan <sdhqaa@naver.com>
2024-02-06 00:11:28 +0900

    Util 수정

--- util.cs
+++ util.cs
@@ -1,3 +1,15 @@
 // var f = () => { return Console.ReadLine().Split().Select(int.Parse).ToArray(); };
-//Console.Write(new HashSet<string>(Console.ReadLine().Trim().Split()).count);
+// Console.Write(new HashSet<string>(Console.ReadLine().Trim().Split()).count);
 //1152번 Console.Write(Console.ReadLine().Trim().Split(" ").Length);
+
+// int m = 0, j, i = 0, x = 1, y = 1;
+// for (; i++ < 9;)
+// {
+//     var n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+//     for (j = 0; j < 9;)
+//         if (m < n[j++])
+//         {
+//             x = i; y = j; m = n[j - 1];
+//         }
+// }
+// Console.Write(m + "\n" + x + " " + y);
